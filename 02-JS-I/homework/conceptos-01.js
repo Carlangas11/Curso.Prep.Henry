5375e3121b9b86998d67e37cabe289c4fe45b755
Carlangas11 <73184352+Carlangas11@users.noreply.github.com>
2020-11-11 00:00:59 -0300

    Subiendo conceptos, conceptos-01.js

---
+++ 02-JS-I/homework/conceptos-01.js
@@ -0,0 +1,11 @@
+	* Variables: es una forma de almacenamiento donde podemos guardar lo que queramos. Un ejemplo seria un curso, en los cursos tendremos varios alumnos
+				aqui podremos tener: alumno1, alumno2, etc.
+	* Strings: Son bloques de texto, estos representan nombres, paises, ciudades o cualquier texto que se pueda pensar. Para el ejemplo de los alumnos,
+				Podran ser sus nombres: 'Andres', 'Roberto', 'Miguel', etc.
+	* Funciones (argumentos, `return`): Las funciones son acciones que podemos hacer que se ejecuten o ejecuten nuestras variables, los argumentos son parametros a los cuales
+										asignamos a nuestras funciones, el return sera la unica forma en la cual podamos devolver el resultado de una funcion. Para el ejemplo del curso,
+										suponemos el primer dia de clases y queremos que nuestros alumnos se presenten, entonces diremos que los alumnos se deben presentar diciendo hola y su nombre,
+										el argumento en este caso sera el nombre y lo que retornara sera: 'Hola yo soy ' + nombre del alumno.
+	* Declaraciones `if`: sera una condicionante la cual pondremos para que se ejecute lo que queremos, supongamos el ejemplo y queremos que los alumnos se presenten en orden alfabetico, entonces 
+							'Andres' ira antes que 'Roberto', pero 'Roberto' ira despues de 'Miguel'.
+	* Valores booleanos (`true`, `false`): Son respuestas simples, variables que solo contienen 2 opciones, verdero o falso, si o no.
